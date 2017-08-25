8a19d199b1088d34d70091a74fc5ee77c8ea2f5e
npapadopoulos <npapadopoulos@cognity.gr>
2017-08-25 21:22:13 +0300

    Maven Profiles

--- src/main/java/gr/blxbrgld/swarm/configuration/Application.java
+++ src/main/java/gr/blxbrgld/swarm/configuration/Application.java
@@ -3,6 +3,9 @@ package gr.blxbrgld.swarm.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Application's Configuration Class
+ */
 @SpringBootApplication
 public class Application {
 
